e5ace6a522ed0612bc444f9ec86cc9bda328ef38
Jhonata Emanuel <xxxonata@gmail.com>
2019-02-23 19:55:30 -0300

    Front, recebendo dados da API

---
+++ js/main.js
@@ -0,0 +1,8 @@
+/*Evintando acesso externo*/
+(function() {
+	const search = document.getElementById('#search');
+	const profile = document.getElementById('#profile');
+	const url = "https://api.github.com/users/";
+	const client_id = "e53febbd554644d70b00";
+	const clinet_secret = "0eabb5d3af6d3ae419c42913f660f0523a19140e";
+})();
